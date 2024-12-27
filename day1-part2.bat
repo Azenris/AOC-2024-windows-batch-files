e036c6f765d3fcb43dd03b8e2773d38475bbef24
Azenris <morleyx22@hotmail.com>
2024-12-27 20:48:24 +0000

    Day1-Part2

---
+++ day1-part2.bat
@@ -0,0 +1,34 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+set input=%~dp0input-day1.txt
+
+set /a numCount = 0
+
+for /f "eol= tokens=1-2 delims= " %%a in (%input%) do (
+	set /a left[!numCount!]=%%a
+	set /a numCount+=1
+
+	if not "!right[%%b]!" == "" (
+		set /a right[%%b] += 1
+	) else (
+		set /a right[%%b] = 1
+	)
+)
+
+set /a total = 0
+set /a numCount -= 1
+
+for /l %%a in ( 0, 1, %numCount% ) do (
+
+	for %%i in (!left[%%a]!) do set rightValue=!right[%%i]!
+
+	if not "!rightValue!" == "" (
+		set /a temp = !left[%%a]! * !rightValue!
+		set /a total += !temp!
+	)
+)
+
+echo TOTAL: %total%
+
+exit /b
